c343fe0b6924a9f48cb85ffa4c247cd75f618d54
Hyosssssun <hyosssssun@gmail.com>
2022-02-12 21:38:31 +0000

    separate backend and frontend

--- frontend/src/Components/Item/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Item() {
-    return <h2>Hi i am Item</h2>;
-}
--- frontend/src/Components/List/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function List() {
-    return (
-        <h2>Hi i am list</h2>
-    )
-};
